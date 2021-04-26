cddaf922ef4cda7d441e728b524b7bcc9ebcd167
dipika fatale <fatale.dipika@gmail.com>
2021-04-26 13:44:44 -0700

    template

---
+++ inc/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Event | Ride </title>
+    <!-- favicon -->
+    <link href="img/favicon5.png" rel=icon>
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar">
+<div id="main-wrapper">
+    <!-- Page Preloader -->
+    <div id="preloader">
+        <div id="status">
+            <div class="status-mes"></div>
+        </div>
+    </div>
+
+    <header class="header">
+        <!-- Navigation -->
+        <nav class="navbar main-menu" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo.png" alt=""></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden"><a href="#page-top"></a></li>
+                        <li><a href="#">BLOG</a></li>
+                        <li><a href="#">SIGN UP</a></li>
+                        <li><a href="#">LOG IN</a></li>                    
+                        <li><a href="#" class="create-event">CREATE EVENTS</a></li>                    
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+        <!-- .nav -->
+    </header>
---
+++ index.php
@@ -0,0 +1,321 @@
+<?php include('inc/header.php');?>
+
+    <div class="jumbotron text-center">
+<div class="content sell-content">
+<div class="panels panel-default">
+<h1>Buy and Sell Your Tickets</h1>
+<h3>Come and Take a Ride with Us</h3>
+</div></div>
+           
+        <div class="content sell-con">
+           <div class="panels panel-default">
+            <div class="panel-body">
+              <div class="row Search-bar">
+                <div class="col-md-12" style="text-align: -webkit-center;">
+                  <div class="form-horizontal">
+                    <div class="input-group search-bar-text-con">
+<div class="ddl-select input-group-btn select-icon-grid">
+<i class="fa fa-th-large" aria-hidden="true"></i>
+</div>
+
+
+                      <div class="ddl-select input-group-btn styled-select">
+                        <select id="ddlsearch" class="selectpicker form-control all-search" data-style="btn-primary" style="padding-left: 5px;padding-right: 0px;">
+                         <option value="" data-hidden="true" class="ddl-title">All</option>
+                          <option value="google">Google</option>
+                          <option value="yahoo">Yahoo</option>
+                          <option value="bing">Bing</option>
+                        </select>
+                      </div>
+                      <input id="txtkey" type="text" class="form-controls search-bar-con" placeholder="Search Tickets For Events" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Tickets For Events'" style=" padding: 10px 11px 9px 10px; border: none;width: 100%;">
+                      <div class="ddl-select input-group-btn styled-select">
+                        <select id="ddlsearch" class="selectpicker form-control search-bar-con" data-style="btn-primary" style="padding: 0px 5px 0px 10px;margin-left: 5px;">
+                          <option value="" data-hidden="true" class="ddl-title">All Dates</option>
+                          <option value="5-4-2017">5-4-2017</option>
+                          <option value="6-4-2017">6-4-2017</option>
+                          <option value="7-4-2017">7-4-2017</option>
+                        </select>
+                      </div>
+                      <span class="input-group-btn all-search-text-bar">
+                        <button id="btn-search" class="btn btn-info all-search all-search-event" type="button" style=""><i class="fa fa-search" aria-hidden="true"></i></button>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+            <!-- <button type="button" class="btn btn-default btn-lg">Search Tickets For Events</button> -->
+        </div>
+    </div>
+    <!-- .Jumbotron-->
+
+<section id="section-speaker" class="section-wrapper team event-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title event-section-title">
+                        <img src="img/Our-events.png" class="img-responsive" alt="Event" style="float: left;"><h1>Our Upcoming Events</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-md-4">
+                    <figure class="thumbnail">
+                        <a href="#"><img src="img/event1.jpg" class="img-responsive" alt="Event"></a>
+                        <figcaption class="caption">
+                            <h3><div class="boots">Boots and Hearts 2017</div>
+                                <small>08/23/2016 at 20.00-22.00 on</small>
+                                <small>Manhanttan / New York</small>
+                                <small class="tickets">Tickets from $52</small>
+                            </h3>                            
+                        </figcaption>
+                    </figure>
+                </div>
+                <!-- /.col-sm-6 -->
+
+                <div class="col-sm-4 col-md-4">
+                    <figure class="thumbnail">
+                        <a href="#"><img src="img/event2.jpg" class="img-responsive" alt="Event"></a>
+                        <figcaption class="caption">
+                            <h3><div class="boots">Boots and Hearts 2017</div>
+                                <small>08/23/2016 at 20.00-22.00 on</small>
+                                <small>Manhanttan / New York</small>
+                                <small class="tickets">Tickets from $52</small>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <!-- /.col-sm-6 -->
+
+                <div class="col-sm-4 col-md-4">
+                    <figure class="thumbnail">
+                        <a href="#"><img src="img/event3.jpg" class="img-responsive" alt="Event"></a>
+                        <figcaption class="caption ">
+                            <h3><div class="boots">Boots and Hearts 2017</div>
+                                <small>08/23/2016 at 20.00-22.00 on</small>
+                                <small>Manhanttan / New York</small>
+                                <small class="tickets">Tickets from $52</small>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <!-- /.col-sm-6 -->
+
+            </div>
+            <!-- .row -->
+            <div class="row">
+                <div class="col-sm-4 col-md-4">
+                    <figure class="thumbnail">
+                        <a href="#"><img src="img/event3.jpg" class="img-responsive" alt="Event"></a>
+                        <figcaption class="caption ">
+                            <h3><div class="boots">Boots and Hearts 2017</div>
+                                <small>08/23/2016 at 20.00-22.00 on</small>
+                                <small>Manhanttan / New York</small>
+                                <small class="tickets">Tickets from $52</small>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <!-- /.col-sm-6 -->
+
+                <div class="col-sm-4 col-md-4">
+                    <figure class="thumbnail">
+                        <a href="#"><img src="img/event1.jpg" class="img-responsive" alt="Event"></a>
+                        <figcaption class="caption ">
+                            <h3><div class="boots">Boots and Hearts 2017</div>
+                                <small>08/23/2016 at 20.00-22.00 on</small>
+                                <small>Manhanttan / New York</small>
+                                <small class="tickets">Tickets from $52</small>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <!-- /.col-sm-6 -->
+
+                <div class="col-sm-4 col-md-4">
+                    <figure class="thumbnail">
+                        <a href="#"><img src="img/event2.jpg" class="img-responsive" alt="Event"></a>
+                        <figcaption class="caption">
+                            <h3><div class="boots">Boots and Hearts 2017</div>
+                                <small>08/23/2016 at 20.00-22.00 on</small>
+                                <small>Manhanttan / New York</small>
+                                <small class="tickets">Tickets from $52</small>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <!-- /.col-sm-6 -->              
+
+            </div>
+            <div class="row see-all-events">
+             <a href="#" class="btn btn-primary">See All Events</a>
+            </div>
+            <!-- .row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- .team -->
+
+    <!-- Start Service -->
+
+    <section id="section-pricing" class="section-wrapper pricing-section service-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="section-title event-section-title">
+                        <img src="img/Our-events.png" class="img-responsive" alt="Event" style="float: left;"><h1>Our Services</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5 col-sm-6 col-md-offset-1">
+                    <div class="Service-content">
+                    <div class="col-md-4 col-sm-4 ser-text"><img src="img/Services2.png" class="img-responsive service-img-con" alt="Service"></div>
+                    <div class="col-md-8 col-sm-8 ser-text"><h1>Create An Events</h1><img src="img/bar.png" class="bar" alt="Bar">
+                    <p>Build an event page that looks dead  sexy on mobile and on your computer. No contracts, no start-up fees, just great experiences for you and your customers. </p></div>
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-5 -->
+                    <div class="col-md-5 col-sm-6">
+                    <div class="Service-content">
+                    <div class="col-md-4 col-sm-4 ser-text"><img src="img/Services1.png" class="img-responsive service-img-con" alt="Service"></div>
+                    <div class="col-md-8 col-sm-8 ser-text"><h1>Add features</h1><img src="img/bar.png" class="bar" alt="Bar">
+                    <p>verything you need to manage ticket sales from start to finish. See your ticket sales data and redemption information reports the way you want to with our improved reporting.</p></div>
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-5 -->
+            </div>
+            <div class="row">
+                <div class="col-md-5 col-sm-6 col-md-offset-1">
+                    <div class="Service-content">
+                    <div class="col-md-4 col-sm-4 ser-text"><img src="img/Services4.png" class="img-responsive service-img-con" alt="Service"></div>
+                    <div class="col-md-8 col-sm-8 ser-text"><h1>Sell Ticket Any Where</h1><img src="img/bar.png" class="bar" alt="Bar">
+                    <p>Free built-in tools make it easy to share your event through social media & email. Use the new Upsell feature for ticket upgrades, or to sell parking and merchandise right as customers </p></div>                 
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-5 -->
+                    <div class="col-md-5 col-sm-6">
+                    <div class="Service-content">
+                    <div class="col-md-4 col-sm-4 ser-text"><img src="img/Services3.png" class="img-responsive service-img-con" alt="Service"></div>
+                    <div class="col-md-8 col-sm-8 ser-text"><h1>Our Fees Struture</h1><img src="img/bar.png" class="bar" alt="Bar">
+                    <p>Free built-in tools make it easy to share your event through social media & email. Use the new Upsell feature for ticket upgrades, or to sell parking and merchandise right as  </p></div>                   
+
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-5 -->
+            </div>
+            <!-- /.row -->
+            <div class="height-line"></div>
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- End Service -->
+    <!-- Start Footer -->
+
+    <section id="section-pricing" class="section-wrapper pricing-section footer-bg">
+        <div class="container containers">           
+            <div class="row">
+                <div class="col-sm-3 col-md-3">
+                    <div class="footer-title">
+                        <div class="pricing-head">
+                            <h3>Contact Us</h3>
+                        </div>
+                        <div class="pricing-detail">
+                            <ul class="pricing-list pink-i">
+                                <li>Company Name</li>
+                                <li>Street Name & Number</li>
+                                <li>Town</li>
+                                <li>Postcode / Zip</li>
+                                <li>Tel : xxxxxxxxx</li>
+                                <li>Fax : xxxxxxxxx</li>
+                                <li>Email : Contact@mydomin.com</li>
+                            </ul>
+                        </div>                        
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-3 -->       
+                <div class="col-sm-3 col-md-3">
+                    <div class="footer-title">
+                        <div class="pricing-head">
+                            <h3>Quick Links</h3>
+                        </div>
+                        <div class="pricing-detail">
+                            <ul class="pricing-list pink-i Links">
+                                <li><a href="#">Link Text Here</a></li>
+                                <li><a href="#">Link Text Here</a></li>
+                                <li><a href="#">Link Text Here</a></li>
+                                <li><a href="#">Link Text Here</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-3 -->                
+                <div class="col-sm-3 col-md-3">
+                    <div class="footer-title">
+                        <div class="pricing-head">
+                            <h3>From The Blog</h3>
+                        </div>
+                        <div class="pricing-detail">
+                            <div class="pricing-list pink-i">
+                                <b>Blog Post Title</b>
+                                <p>Posted By Admin on. 00 00. 0000</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <p class="pull-right"><a href="#">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-3 -->                
+                <div class="col-sm-3 col-md-3">
+                    <div class="footer-title">
+                        <div class="pricing-head">
+                            <h3>Grab Our Newsletter</h3>
+                        </div>
+                        <div class="pricing-detail">
+                        <form method="post" action="#" class="footer-form">
+                              <div class="form-group">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                              </div>
+                              <div class="form-group">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                              </div>                              
+                              <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                            
+                        </div>
+                    </div>
+                    <!-- /.pricing-table-wrapper -->
+                </div>
+                <!-- .col-sm-3 -->                         
+            </div>
+
+            <!-- Social Icon -->
+            <div class="row Social-icon">
+            <div class="social-icon-img pull-right">
+            <ul>
+                <li><a href="#"><img src="img/f.png" alt="Facebook"></a></li>
+                <!--<li><a href="#"><img src="img/b.png" alt="Blog"></a></li>-->
+                <li><a href="#"><img src="img/t.png" alt="Facebook"></a></li>
+                <li><a href="#"><img src="img/g.png" alt="Facebook"></a></li>
+                <li><a href="#"><img src="img/gp.png" alt="Facebook"></a></li>
+                <li><a href="#"><img src="img/ic.png" alt="Facebook"></a></li>
+            </ul>
+            </div>
+            </div>
+            <!-- Social Icon -->
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- .pricing-section -->
+    <!-- End Footer -->
+
+<?php include('inc/footer.php');?>
